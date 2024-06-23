97db2c16a33847f72a55c3121b3a87053055444d
InformacijeProg <63242860+InformacijeProg@users.noreply.github.com>
2024-06-23 22:47:22 +0200

    YoutubeDownloaderGUI

---
+++ main.py
@@ -0,0 +1,19 @@
+import tkinter
+import customtkinter
+from pytube import Youtube
+
+# System settings
+
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
+# Our app frame
+
+app = customtkinter.CTk()
+app.geometry("720x480")
+app.title("Youtube Downloader")
+
+
+
+# Run app
+app.mainloop()
